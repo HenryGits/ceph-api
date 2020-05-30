c4e84364cd33461593d234d6675211179484f1fa
HenryGits <809683500@qq.com>
2020-05-30 09:59:16 +0800

    接口返回值

--- system/web/result.go
+++ system/web/result.go
@@ -10,22 +10,29 @@ type ResponseBean struct {
 	Data interface{} `json:"data"`
 }
 
+const (
+	SUCCESS    = 200
+	NotFount   = 404
+	ERROR      = 500
+	BadRequest = 400
+)
+
 func GenSuccess(msg string) *ResponseBean {
-	return &ResponseBean{200, msg, ""}
+	return &ResponseBean{SUCCESS, msg, ""}
 }
 
 func GenSuccessMsg(data interface{}) *ResponseBean {
-	return &ResponseBean{200, "OK", data}
+	return &ResponseBean{SUCCESS, "OK", data}
 }
 
 func GenOperationErrorMsg() *ResponseBean {
-	return &ResponseBean{400, "Bad Request", ""}
+	return &ResponseBean{BadRequest, "Bad Request", ""}
 }
 
 func GenNotFondMsg() *ResponseBean {
-	return &ResponseBean{404, "API Not Found", ""}
+	return &ResponseBean{NotFount, "API Not Found", ""}
 }
 
 func GenFailedMsg(errMsg string) *ResponseBean {
-	return &ResponseBean{500, errMsg, ""}
+	return &ResponseBean{ERROR, errMsg, ""}
 }
